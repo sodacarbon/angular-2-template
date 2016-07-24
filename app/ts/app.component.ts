dd491c7a706101d46e4cb36b9c073d737b8c9975
sodacarbon <sodacarbon@gmail.com>
2016-07-24 23:03:01 +0700

    7.1 Nested Components Added directives: [PlaylistComponent] so we can use <playlist> tag

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from 'angular2/core';
 import {Config} from "./config.service";
+import {PlaylistComponent} from "./playlist.component";
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/ts/app.component.html'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
 export class AppComponent {
