689002dab2c5179bc12a59d7c0c35e9635c9d98e
sodacarbon <sodacarbon@gmail.com>
2016-07-24 23:46:03 +0700

    8.1 Passing Data to Components created a list of videos (array) CODE:   app:     videos:Array<Video>;   playlist:     inputs: ['videos'] HTML:   app:     <playlist [videos]="videos">   playlist:     <tr *ngFor="#v of videos">

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {Config} from "./config.service";
 import {PlaylistComponent} from "./playlist.component";
+import {Video} from "./video";
+import {id} from "angular2/i18n";
 
 @Component({
     selector: 'my-app',
@@ -10,4 +12,12 @@ import {PlaylistComponent} from "./playlist.component";
 
 export class AppComponent {
     mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor() {
+        this.videos = [
+            new Video(1, "Angular 2 for Beginners - Tutorial 6", "pz9sbuMvhQU", "Creating our First Component"),
+            new Video(2, "Angular 2 for Beginners - Tutorial 7", "f8qBeaGe2S4", "Nested Components")
+        ]
+    }
 }
